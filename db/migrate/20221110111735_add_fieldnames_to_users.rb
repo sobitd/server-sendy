3c479d0b031aeb575ed2708b7b885abf3d026ef2
sobit <sobitdaniel@gmail.com>
2022-11-10 14:23:00 +0300

    edit users table add columns

---
+++ db/migrate/20221110111735_add_fieldnames_to_users.rb
@@ -0,0 +1,9 @@
+class AddFieldnamesToUsers < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string 
+    add_column :users, :phone_number, :string 
+    add_column :users, :username, :string
+    add_column :users, :isAdmin, :integer, default: 0
+  end
+end
